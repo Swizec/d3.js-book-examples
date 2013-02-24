cca9e8b0b04ba91aaba1870a2e80111820f837a0
Swizec Teller <swizec@swizec.com>
2013-02-24 23:41:32 +0100

    rendering airports

--- ch3/geography.js
+++ ch3/geography.js
@@ -8,7 +8,7 @@ var width = 1800,
 
 var projection = d3.geo.equirectangular()
         .center([8, 56])
-        .scale(800);
+        .scale(1200);
 
 queue()
     .defer(d3.json, 'data/water.json')
@@ -39,6 +39,60 @@ function draw (err, water, land, cultural) {
     add_to_map(cultural, 'ne_50m_admin_0_boundary_lines_land')
         .classed('boundary', true);
 
-    add_to_map(cultural, 'ne_10m_urban_areas')
-        .classed('urban', true);
+//    add_to_map(cultural, 'ne_10m_urban_areas')
+//        .classed('urban', true);
+
+    add_airlines();
 };
+
+
+function add_airlines() {
+    queue()
+        .defer(d3.text, 'data/airports.dat')
+        .defer(d3.text, 'data/routes.dat')
+        .await(draw_airlines);
+};
+
+function draw_airlines(err, _airports, _routes) {
+    var airports = {},
+        routes = {};
+
+    d3.csv.parseRows(_airports).forEach(function (airport) {
+        var id = airport[0];
+
+        airports[id] = {
+            lat: airport[6],
+            lon: airport[7]
+        };
+    });
+
+    d3.csv.parseRows(_routes).forEach(function (route) {
+        var from_airport = route[3];
+
+        if (!routes[from_airport]) {
+            routes[from_airport] = [];
+        }
+
+        routes[from_airport].push({
+                to: routes[5],
+                stops: routes[7]
+        });
+    });
+
+    var route_N = d3.values(routes).map(function (routes) {
+        return routes.length;
+    }),
+        R = d3.scale.linear().domain([d3.min(route_N), d3.max(route_N)]).range([2, 15]);
+    
+   svg.append('g')
+        .selectAll('circle')
+        .data(d3.keys(airports))
+        .enter()
+        .append('circle')
+        .attr("transform", function (id) {
+            var airport = airports[id];
+            return "translate("+projection([airport.lon, airport.lat])+")";
+        })
+        .attr('r', function (id) { return routes[id] ? R(routes[id].length) : 1; })
+        .classed('airport', true);
+}
