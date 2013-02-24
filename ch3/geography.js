6e6c872a77f619657e326cae76e9552cdc533e83
Swizec Teller <swizec@swizec.com>
2013-02-24 17:18:56 +0100

    map of world

--- ch3/geography.js
+++ ch3/geography.js
@@ -1,6 +1,6 @@
 
 var width = 1200,
-    height = 800,
+    height = 1200,
     svg = d3.select('#graph')
         .append('svg')
         .attr({width: width,
@@ -8,15 +8,37 @@ var width = 1200,
 
 var projection = d3.geo.equirectangular()
         .center([14, 46])
-        .scale(1000);
+        .scale(1400);
 
-var colors = d3.scale.ordinal().range(colorbrewer.RdBu[8]);
+queue()
+    .defer(d3.json, 'data/water.json')
+    .defer(d3.json, 'data/land.json')
+    .defer(d3.json, 'data/cultural.json')
+    .await(draw);
 
-d3.json('countries.json', function (countries) {
-    svg.selectAll('path')
-        .data(topojson.object(countries, countries.objects['ne_10m_admin_0_countries']).geometries)
+function add_to_map(collection, key) {
+    return svg.append('g')
+        .selectAll('path')
+        .data(topojson.object(collection, 
+                              collection.objects[key]).geometries)
         .enter()
         .append('path')
-        .attr('d', d3.geo.path().projection(projection))
-        .style('fill', function (d, i) { return colors(i); });
-});
+        .attr('d', d3.geo.path().projection(projection));
+}
+
+function draw (err, water, land, cultural) {
+   add_to_map(water, 'ne_50m_ocean')
+        .classed('ocean', true);
+
+    add_to_map(land, 'ne_50m_land')
+        .classed('land', true);
+
+    add_to_map(water, 'ne_50m_rivers_lake_centerlines')
+        .classed('river', true);
+
+    add_to_map(cultural, 'ne_50m_admin_0_boundary_lines_land')
+        .classed('boundary', true);
+
+    add_to_map(cultural, 'ne_10m_urban_areas')
+        .classed('urban', true);
+};
