17e866b14b18060e106dc8bed8b0309f003d19ec
Swizec Teller <swizec@swizec.com>
2013-03-12 23:27:18 +0100

    parsing raw irc dump into a sane karma matrix

--- ch5/data/parse_irc.py
+++ ch5/data/parse_irc.py
@@ -3,17 +3,41 @@ import json
 import os
 import re
 
-karma = re.compile("\w{2,}\+\+")
+karma = re.compile("\w{2,10}\+\+|\+\+\w{2,10}")
+extension = re.compile("\W+$")
 
 def parse(entry):
     return {'time': entry['time'],
             'from': entry['nick'],
-            'to': re.findall(karma, entry['message'])[0]}
+            'to': re.findall(karma, entry['message'])[0].replace('+', '')}
+
+def normalize(entries):
+    # bare form to first occurence map
+    nicks = {}
+
+    for entry in entries:
+        bare_from = re.sub(extension, '', entry['from'].lower())
+        bare_to = re.sub(extension, '', entry['to'].lower())
+
+        if not bare_from in nicks:
+            nicks[bare_from] = entry['from']
+
+        if not bare_to in nicks:
+            nicks[bare_to] = entry['to']
+
+        entry['from'] = nicks[bare_from]
+        entry['to'] = nicks[bare_to]
+
+        yield entry
 
 if __name__ == '__main__':
     file = os.path.join(
         os.path.split(os.path.realpath(__file__))[0],
         'irc_dump.json')
 
-    print [parse(entry) for entry in json.loads(open(file).read())
-           if '++' in entry['message']][30]
+    karmas = normalize(parse(entry) for entry in json.loads(open(file).read())
+                       if re.match(karma, entry['message']))
+
+    open(os.path.join(
+        os.path.split(os.path.realpath(__file__))[0],
+        'karma_matrix.json'), 'w').write(json.dumps(list(karmas)))
