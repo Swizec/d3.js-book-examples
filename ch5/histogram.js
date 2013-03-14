ddf89a97c8eb39118ddec48f0749ac7e513951e2
Swizec Teller <swizec@swizec.com>
2013-03-14 20:28:49 +0100

    histogram labeled with names

--- ch5/histogram.js
+++ ch5/histogram.js
@@ -10,7 +10,8 @@ var width = 1024,
 
 d3.json('data/karma_matrix.json', function (data) {
 
-    var uniques = [];
+    var uniques = [],
+        bottom_pad = 100;
     
     data.forEach(function (d) {
         if (uniques.indexOf(d.to) < 0) {
@@ -18,28 +19,39 @@ d3.json('data/karma_matrix.json', function (data) {
         }
     });
 
-    var nick = d3.scale.ordinal().domain(uniques).range(d3.range(uniques.length));
+    var nick_id = d3.scale.ordinal().domain(uniques).range(d3.range(uniques.length));
 
     var histogram = d3.layout.histogram()
-            .bins(nick.range())
-            .value(function (d) { return nick(d.to); })(data);
+            .bins(nick_id.range())
+            .value(function (d) { return nick_id(d.to); })(data);
 
     var x = d3.scale.linear()
             .domain([0, d3.max(histogram, function (d) { return d.x; })])
             .range([0, width]),
         y = d3.scale.linear()
             .domain([0, d3.max(histogram, function (d) { return d.y; })])
-            .range([height, 0]);
+            .range([height-bottom_pad, 0]);
 
-    console.log(histogram);
+    var bar = svg.selectAll('.bar')
+            .data(histogram)
+            .enter()
+            .append('g')
+            .classed('bar', true)
+            .attr('transform', 
+                  function (d) { return 'translate('+x(d.x)+', '+y(d.y)+')'; });
 
-    svg.selectAll('rect')
-        .data(histogram)
-        .enter()
-        .append('rect')
-        .attr({x: function (d) { return x(d.x)+1; },
-               y: function (d) { return y(d.y); },
+    bar.append('rect')
+        .attr({x: 1,
                width: x(histogram[0].dx)-1,
-               height: function (d) { return height-y(d.y); }
+               height: function (d) { return height-bottom_pad-y(d.y); }
               });
+
+    bar.append('text')
+        .text(function (d) { return d[0].to; })
+        .attr({transform: function (d) {
+                   var bar_height = height-bottom_pad-y(d.y);
+
+                   return 'translate(0, '+(bar_height+7)+') rotate(60)'; }
+        });
+        
 });
