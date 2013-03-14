cc5ccc8f37fd12a1569d14a7a3101dd4a14a658b
Swizec Teller <swizec@swizec.com>
2013-03-14 19:25:23 +0100

    a histogram renders

--- ch5/histogram.js
+++ ch5/histogram.js
@@ -1,20 +1,45 @@
 
 var width = 1024,
-    height = 768,
+    height = 600,
     svg = d3.select('#graph')
         .append('svg')
         .attr({width: width,
-               height: height});
+               height: height})
+        .style('margin', '20px');
 
 
 d3.json('data/karma_matrix.json', function (data) {
 
-    var receivers = data.map(function (d) {
-            return d.to;
-        });
+    var uniques = [];
     
-    // very much designed to work with numbers not ordinal things ...
-    var histogram = d3.layout.histogram()(receivers);
-    
+    data.forEach(function (d) {
+        if (uniques.indexOf(d.to) < 0) {
+            uniques.push(d.to);
+        }
+    });
+
+    var nick = d3.scale.ordinal().domain(uniques).range(d3.range(uniques.length));
+
+    var histogram = d3.layout.histogram()
+            .bins(nick.range())
+            .value(function (d) { return nick(d.to); })(data);
+
+    var x = d3.scale.linear()
+            .domain([0, d3.max(histogram, function (d) { return d.x; })])
+            .range([0, width]),
+        y = d3.scale.linear()
+            .domain([0, d3.max(histogram, function (d) { return d.y; })])
+            .range([height, 0]);
+
     console.log(histogram);
+
+    svg.selectAll('rect')
+        .data(histogram)
+        .enter()
+        .append('rect')
+        .attr({x: function (d) { return x(d.x)+1; },
+               y: function (d) { return y(d.y); },
+               width: x(histogram[0].dx)-1,
+               height: function (d) { return height-y(d.y); }
+              });
 });
