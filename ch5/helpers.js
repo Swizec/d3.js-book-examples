25c13e1c9653924e3c36264ac863730d3e7792e9
Swizec Teller <swizec@swizec.com>
2013-03-18 23:21:30 +0100

    labeled subgroups

--- ch5/helpers.js
+++ ch5/helpers.js
@@ -35,5 +35,24 @@ window.helpers = {
         .range(['#EF3B39', '#FFCD05', '#69C9CA', '#666699', '#CC3366', '#0099CC', 
                 '#CCCB31', '#009966', '#C1272D', '#F79420', '#445CA9', '#999999', 
                 '#402312', '#272361', '#A67C52', '#016735', '#F1AAAF', '#FBF5A2', 
-                '#A0E6DA', '#C9A8E2', '#F190AC', '#7BD2EA', '#DBD6B6', '#6FE4D0'])
+                '#A0E6DA', '#C9A8E2', '#F190AC', '#7BD2EA', '#DBD6B6', '#6FE4D0']),
+
+    arc_labels: function (selection, text, radius) {
+        return selection.append('text')
+            .text(text)
+            .attr('text-anchor', function (d) {
+                return helpers.tickAngle(d) > 100 ? 'end' : 'start';
+            })
+            .attr('transform', function (d) {
+                var degrees = helpers.tickAngle(d);
+                
+                var turn = 'rotate('+degrees+') translate('+(radius(d)+10)+', 0)';
+                
+                if (degrees > 100) {
+                    turn += 'rotate(180)';
+                }
+                
+                return turn;
+            });
+    }
 };
--- ch5/partition.js
+++ ch5/partition.js
@@ -43,17 +43,30 @@ d3.json('data/karma_matrix.json', function (data) {
 
     var arc = d3.svg.arc()
             .innerRadius(function (d) { return radius(d.y); })
-            .outerRadius(function (d) { return radius(d.y+d.dy); })
-            .startAngle(function (d) { return d.x; })
-            .endAngle(function (d) { return d.x+d.dx; });
+            .outerRadius(function (d) { return d.depth ? radius(d.y+d.dy/d.depth) : 0; });
+
+    nodes = nodes.map(function (d) {
+        d.startAngle = d.x;
+        d.endAngle = d.x+d.dx;
+        return d;
+    });
+
+    nodes = nodes.filter(function (d) { return d.depth; });
 
     var chart = svg.append('g')
             .attr('transform', 'translate('+width/2+','+height/2+')');
 
-    chart.selectAll('path')
-        .data(nodes)
-        .enter()
-        .append('path')
+    var node = chart.selectAll('g')
+            .data(nodes)
+            .enter()
+            .append('g');
+    
+    node.append('path')
         .attr({d: arc,
                fill: function (d) { return d.depth ? helpers.color(d.nick) : 'white'; }});
+    
+    helpers.arc_labels(node.filter(function (d) { return d.depth > 1; }),
+                       function (d) { return d.nick; },
+                       arc.outerRadius());
+
 });
